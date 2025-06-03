ea9836ee4830f544b79aa93d2d5767f617c58cc6
yuyu-yuyu-yuyu-0001 <yuyu.yuyu.yuyu.0001@gmail.com>
2025-06-03 13:36:35 +0800

    Update 300.py

--- 300.py
+++ 300.py
@@ -10,7 +10,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 
-cred = credentials.Certificate("firebase_service_account.json")
+cred = credentials.Certificate("firebase-key.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 user_collection = db.collection('line_users')

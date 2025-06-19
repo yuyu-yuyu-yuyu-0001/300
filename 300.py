5383b2ebae0ace502fe2633b18be780caae15fd4
yuyu-yuyu-yuyu-0001 <yuyu.yuyu.yuyu.0001@gmail.com>
2025-06-19 17:14:44 +0800

    Update 300.py

--- 300.py
+++ 300.py
@@ -11,6 +11,13 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from datetime import datetime
 from mega import Mega
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+from langchain.schema import Document
 
 
 firebase_key_json = os.environ.get("FIREBASE_CREDENTIALS")

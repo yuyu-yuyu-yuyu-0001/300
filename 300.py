9d68edaaed9786a88cac9a41908bceda022f8e4d
yuyu-yuyu-yuyu-0001 <yuyu.yuyu.yuyu.0001@gmail.com>
2025-06-21 00:58:46 +0800

    Update 300.py

--- 300.py
+++ 300.py
@@ -37,7 +37,7 @@ handler = WebhookHandler(CHANNEL_SECRET)
 
 
 def load_embedding_model():
-    return HuggingFaceEmbeddings(model_name="sentence-transformers/TinyBERT-SentenceEmbedding")
+    return HuggingFaceEmbeddings(model_name="shibing624/text2vec-base-multilingual")
 
 # === STEP 2: 讀取 PDF 檔 ===
 def load_documents(filepath: str):
